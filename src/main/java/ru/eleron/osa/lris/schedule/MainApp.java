b2292950aab25796aa3a6ecd65fa44ee1ccef480
reyzor <testaccwow001@gmail.com>
2018-04-16 15:45:58 +0300

    main class added

---
+++ src/main/java/ru/eleron/osa/lris/schedule/MainApp.java
@@ -0,0 +1,15 @@
+package ru.eleron.osa.lris.schedule;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application  {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("Hello world");
+    }
+}
