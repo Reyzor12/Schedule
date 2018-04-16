20560693a15b5ce48b8bb737bf576e0724524083
Reyzor <testaccwow001@gmail.com>
2018-04-16 21:53:51 +0300

    config didn't done

--- src/main/java/ru/eleron/osa/lris/schedule/configurations/JpaConfigurations.java
+++ src/main/java/ru/eleron/osa/lris/schedule/configurations/JpaConfigurations.java
@@ -1,7 +1,46 @@
 package ru.eleron.osa.lris.schedule.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 
 @Configuration
+@EnableJpaRepositories(basePackages = "ru.eleron.osa.lris.schedule.database", entityManagerFactoryRef = "sessionFactory")
+@PropertySource("db.properties")
+@EnableTransactionManagement
+@ComponentScan(basePackages = {"ru.eleron.osa.lris.schedule"})
 public class JpaConfigurations {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+        dataSource.setUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.username"));
+        dataSource.setPassword(environment.getProperty("jdbc.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean sessionFactory() {
+        LocalContainerEntityManagerFactoryBean sessionFactory = new LocalContainerEntityManagerFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        sessionFactory.setJpaProperties(additionalProperties());
+        return sessionFactory;
+    }
 }
