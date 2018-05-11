c7cfbf77f8cac721a17ce2abf9388824e768044f
reyzor <testaccwow001@gmail.com>
2018-05-11 15:45:19 +0300

    graphic of fxml done todo controller

--- src/main/java/ru/eleron/osa/lris/schedule/controllers/CreateScheduleMenuController.java
+++ src/main/java/ru/eleron/osa/lris/schedule/controllers/CreateScheduleMenuController.java
@@ -1,9 +1,14 @@
 package ru.eleron.osa.lris.schedule.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 import ru.eleron.osa.lris.schedule.utils.frame.FrameControllerBaseIF;
+import ru.eleron.osa.lris.schedule.utils.storage.ConstantsForElements;
 
 /**
  * Controller for creating templates of schedule
@@ -16,8 +21,16 @@ import ru.eleron.osa.lris.schedule.utils.frame.FrameControllerBaseIF;
 @Component
 public class CreateScheduleMenuController implements FrameControllerBaseIF
 {
-
     private static final Logger logger = LogManager.getLogger(CreateScheduleMenuController.class);
+
+    @FXML
+    private Button leftRemoveButton;
+    @FXML
+    private Button rightAddButton;
+    @FXML
+    private ImageView leftImage;
+    @FXML
+    private ImageView rightImage;
 
     public void initialize()
     {
@@ -40,5 +53,22 @@ public class CreateScheduleMenuController implements FrameControllerBaseIF
     @Override
     public void enableTooltips() {
 
+    }
+
+    public void focusLeftButton()
+    {
+        leftImage.setImage(new Image(getClass().getClassLoader().getResource(ConstantsForElements.ARROW_LEFT_BLACK.getMessage()).toString()));
+    }
+    public void focusLostLeftButton()
+    {
+        leftImage.setImage(new Image(getClass().getClassLoader().getResource(ConstantsForElements.ARROW_LEFT.getMessage()).toString()));
+    }
+    public void focusRightButton()
+    {
+        rightImage.setImage(new Image(getClass().getClassLoader().getResource(ConstantsForElements.ARROW_RIGHT_BLACK.getMessage()).toString()));
+    }
+    public void focusLostRightButton()
+    {
+        rightImage.setImage(new Image(getClass().getClassLoader().getResource(ConstantsForElements.ARROW_RIGHT.getMessage()).toString()));
     }
 }
--- src/main/java/ru/eleron/osa/lris/schedule/utils/storage/ConstantsForElements.java
+++ src/main/java/ru/eleron/osa/lris/schedule/utils/storage/ConstantsForElements.java
@@ -9,10 +9,22 @@ package ru.eleron.osa.lris.schedule.utils.storage;
 
 public enum ConstantsForElements
 {
+    /**
+     * Placeholder for tables
+     * */
 
     EMPTY_CURRENT_TASK_TABLE("Задачи на сегодня отсутствуют"),
     EMPTY_TASK_TEMPLATES("Шаблоны задач отсутствуют"),
     EMPTY_COMPOSITE_TASK("Задачи для шаблона отсутствуют"),
+
+    /**
+     * Path to arrow images
+     * */
+
+    ARROW_LEFT("images/icons/left.png"),
+    ARROW_RIGHT("images/icons/right.png"),
+    ARROW_LEFT_BLACK("images/icons/left-black.png"),
+    ARROW_RIGHT_BLACK("images/icons/right-black.png"),
 
     /**
      * Path to images for spinner
